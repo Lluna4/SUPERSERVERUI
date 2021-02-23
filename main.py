dc8adf1a6ab6eb995c0f4dd4654b60d46762ed78
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-02-23 23:49:17 +0100

    Errores arregados, y funciona?

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 import os
 import zipfile
+import requests
+import socket
 
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile("DBCRD.txt")
@@ -66,17 +68,48 @@ with open("server/server.properties", "w") as propedades:
         propedades.write("\ngamemode=1")
     if i3 == "aventura":
         propedades.write("\ngamemode=2")
+    ip = socket.gethostbyname(socket.gethostname())
+
+    propedades.write(f"\nip={ip}")
 
 
-i4 = input("Quieres que el mundo se suba automaticamente a drive y se descargue cuando se abra el servidor? si dices que si, se te preguntara la cuenta de google")
+i4 = input("Quieres que el mundo se suba automaticamente a drive y se descargue cuando se abra el servidor? si dices que si, se te preguntara la cuenta de google ")
 
 if i4 == "si":
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
     gauth.SaveCredentialsFile("DBCRD.txt")
+    drive = GoogleDrive(gauth)
+    world = drive.CreateFile({'title': 'world.txt'})
+    world.Upload()
+    world_nether = drive.CreateFile({'title': 'world_nether.txt'})
+    world_nether.Upload()
+    world_the_end = drive.CreateFile({'title': 'world_the_end.txt'})
+    world_the_end.Upload()
+    id1 = world.GetPermissions()
+    id1 = str(id1[0])
+    id1 = id1[77:-371]
+
+    id2 = world_nether.GetPermissions()
+    id2 = str(id2[0])
+    id2 = id2[77:-371]
+
+    id3 = world_the_end.GetPermissions()
+    id3 = str(id3[0])
+    id3 = id3[77:-371]
+
+    with open("world_id.txt", "w") as world_id:
+        world_id.write(id1)
+    
+    with open("world_nether_id.txt", "w") as world_nether_id:
+        world_nether_id.write(id2)
+    
+    with open("world_the_end_id.txt", "w") as world_the_end_id:
+        world_the_end_id.write(id3)
+    
     with open("server/start.bat", "w") as start:
         start.write("python descargar_drive.py")
-        start.write("\njava -Xms1G -Xmx3G -jar paper-1.16.4-393.jar -nogui")
+        start.write("\njava -Xms1G -Xmx3G -jar server.jar -nogui")
         start.write("\npython drive_flojito.py")
         start.write("\nPAUSE")
 
